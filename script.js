93a03cfd87daf60a1c3bc521dea9292c89bd0eaf
antoniorusso2 <russoantonio303@outlook.it>
2024-10-27 17:01:12 +0100

    ADD: 2nd snack - for loop for replace object keys value with random int

--- script.js
+++ script.js
@@ -4,25 +4,25 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 
 const bikes = [
-  {
-    nome: 'bici1',
-    peso: 5
-  },
+	{
+		nome: 'bici1',
+		peso: 5
+	},
 
-  {
-    nome: 'bici9',
-    peso: 3
-  },
+	{
+		nome: 'bici9',
+		peso: 3
+	},
 
-  {
-    nome: 'bici4',
-    peso: 8
-  },
+	{
+		nome: 'bici4',
+		peso: 8
+	},
 
-  {
-    nome: 'bici6',
-    peso: 12
-  }
+	{
+		nome: 'bici6',
+		peso: 12
+	}
 ];
 
 //obiettivo = la chiave peso contenuta negli object a loro volta contenuti nell'array bikes
@@ -54,71 +54,62 @@ let minorWeight = bikes[0];
 
 //itero l'array contenente le bici
 for (let i = 0; i < bikes.length; i++) {
-  let currentElement = bikes[i];
-  // console.log(currentElement);
+	let currentBike = bikes[i];
+	// console.log(currentBike);
 
-  //faccio il confronto tra il peso della bici che ho preso con quelle dell'array
-  //se l'elemento corrente dell'iterazione ha un peso minore rispetto alla bici nella variabile esterna al ciclo
-  if (currentElement.peso < minorWeight.peso) {
+	//faccio il confronto tra il peso della bici che ho preso con quelle dell'array
+	//se l'elemento corrente dell'iterazione ha un peso minore rispetto alla bici nella variabile esterna al ciclo
+	if (currentBike.peso < minorWeight.peso) {
 
-    //sovrascrivo il valore della variabile
-    minorWeight = currentElement;
-  }
+		//sovrascrivo il valore della variabile
+		minorWeight = currentBike;
+	}
 }
 
 console.log('La bici con il peso minore:', minorWeight);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 //   Snack2
 // Creare un array di oggetti di squadre di calcio.
 
-// let teams = [ //  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-//   {
-//     nome: 'napoli',
-//     punti_fatti: 0,
-//     falli_subiti: 0
-//   },
-//   {
-//     nome: 'roma',
-//     punti_fatti: 0,
-//     falli_subiti: 0
-//   },
-//   {
-//     nome: 'juventus',
-//     punti_fatti: 0,
-//     falli_subiti: 0
-//   }
-// ];
+//funzione numeri random
+function randomInt() {
+	let num = Math.floor(Math.random() * 30) + 1;
+	return num;
+}
 
-// // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+//  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+let teams = [
+	{
+		nome: 'napoli',
+		punti_fatti: 0,
+		falli_subiti: 0
+	},
+	{
+		nome: 'roma',
+		punti_fatti: 0,
+		falli_subiti: 0
+	},
+	{
+		nome: 'juventus',
+		punti_fatti: 0,
+		falli_subiti: 0
+	}
+];
 
-// // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+//iterazione array
+for (let i = 0; i < teams.length; i++) {
+	//chiave alla quale cambiare valore
+	let obj = teams[i];
 
-// //funzione numeri random
-// function randomInt() {
-//   let num = math.floor(Math.random() * 30) + 1;
-// }
+	obj['punti_fatti'] = randomInt();
+	obj['falli_subiti'] = randomInt();
+}
 
-//funzione aggiunta valori in oggetti
-
-
-
+console.log(teams);
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-
-
 
 //   Snack 3(Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri(a più piccolo di b).
